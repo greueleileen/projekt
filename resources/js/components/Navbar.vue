5890427f6055364db52e7766163a1040b7fc1eb1
Eileen Greuel <eileen.greuel@web.de>
2018-11-18 14:49:08 +0100

    Added frontend Login

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
     <nav class="navbar navbar-dark navbar-expand-sm bg-info mb-2">
         <div class="container">
-            <a href="#" class="navbar-brand">Artikel</a>
-            <a href="#" class="navbar-brand">Benutzer</a>
-            <a href="#" class="navbar-brand">Startseite</a>
+            <a href="" class="navbar-brand">Artikel</a>
+            <a href="" class="navbar-brand">Benutzer</a>
+            <a href="" class="navbar-brand">Startseite</a>
         </div>
     </nav>
 
